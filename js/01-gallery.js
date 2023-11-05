0a5e4b62e24edeb660ca71ed4512343e256962c1
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-05 15:55:48 +0100

    homework 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  let item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </li>`;
+
+  gallery.innerHTML += item;
+});
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const imgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+	<img src = "${imgUrl}" />
+`);
+
+  instance.show();
+  console.log(imgUrl);
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((galleryItem) => {
+  let item = `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+       <img 
+       class="gallery__image" 
+       src="${galleryItem.preview}" 
+       alt="${galleryItem.description}" />
+    </a>
+ </li>`;
+
+  gallery.innerHTML += item;
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const imgUrl = event.target.dataset.source;
+}
